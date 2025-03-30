4cde611656bf28ab3d33d8be2443a633710959b8
21subham <subhamrajbhandari@gmail.com>
2025-03-30 17:08:17 +0545

    minor changes

--- websites/as1/public/header.php
+++ websites/as1/public/header.php
@@ -3,69 +3,91 @@ require 'dbConn.php';
 ?>
 
 <html>
-	<head>
-		<title>Carbuy Auctions</title>
-		<link rel="stylesheet" href="carbuy.css" />
-		<style>
+
+<head>
+	<title>Carbuy Auctions</title>
+	<link rel="stylesheet" href="carbuy.css" />
+	<style>
 		.HeaderButton {
 			display: inline-block;
 			position: relative;
-			
+
 		}
 
 		.HeaderButton-options {
+			min-width: 150px;
+			max-width: 150px;
 			display: none;
 			position: absolute;
 			overflow: auto;
-			
-			
-			
-			
 		}
 
 		.HeaderButton:hover .HeaderButton-options {
 			display: block;
-			
+			color: black;
+			background-color: white;
+			width: 10em;
+			z-index: 1;
+			position: absolute;
+			border: 1px solid #ccc;
 		}
-		
-	</style>
-	</head>
 
-	<body>
+		.HeaderButton-options {
+			display: none;
+			flex-direction: column;
+			padding: 5px;
+		}
+
+		.HeaderButton-options a {
+			display: block;
+			padding: 5px;
+			text-decoration: none;
+			color: black;
+		}
+
+		.HeaderButton-options a:hover {
+			background-color: lightgray;
+		}
+	</style>
+</head>
+
+<body>
 
 	<!-- carbuy + search section -->
-		<header>
-			<a href="index.php">
+	<header>
+		<a href="index.php">
 			<h1><span class="C">C</span>
- 			<span class="a">a</span>
-			<span class="r">r</span>
-			<span class="b">b</span>
-			<span class="u">u</span>
-			<span class="y">y</span></h1></a>
+				<span class="a">a</span>
+				<span class="r">r</span>
+				<span class="b">b</span>
+				<span class="u">u</span>
+				<span class="y">y</span>
+			</h1>
+		</a>
 
-			<form action="#">
-				<input type="text" name="search" placeholder="Search for a car" />
-				<input type="submit" name="submit" value="Search" />
-			</form>
-		</header>
-
-
-		<!-- Category Section -->
-		<nav>
-			<ul>
-				<li><a class="categoryLink" href="#">Estate</a></li>
-				<li><a class="categoryLink" href="#">Electric</a></li>
-				<li><a class="categoryLink" href="#">Coupe</a></li>
-				<li><a class="categoryLink" href="#">Saloon</a></li>
-				<li><a class="categoryLink" href="#">4x4</a></li>
-				<li><a class="categoryLink" href="#">Sports</a></li>
-				<li><a class="categoryLink" href="#">Hybrid</a></li>
-				<li><a class="categoryLink" href="#">More</a></li>
-			</ul>
-		</nav>
+		<form action="#">
+			<input type="text" name="search" placeholder="Search for a car" />
+			<input type="submit" name="submit" value="Search" />
+		</form>
+	</header>
 
 
-		<div class="headerButton" style="justify-content: center">
+	<!-- Category Section -->
+	<nav>
+		<ul>
+			<li><a class="categoryLink" href="#">Estate</a></li>
+			<li><a class="categoryLink" href="#">Electric</a></li>
+			<li><a class="categoryLink" href="#">Coupe</a></li>
+			<li><a class="categoryLink" href="#">Saloon</a></li>
+			<li><a class="categoryLink" href="#">4x4</a></li>
+			<li><a class="categoryLink" href="#">Sports</a></li>
+			<li><a class="categoryLink" href="#">Hybrid</a></li>
+			<li><a class="categoryLink" href="#">More</a></li>
+		</ul>
+	</nav>
+
+
+	<div class="headerButton" style="justify-content: center">
 		<button>Profile</button>
 		<div class="HeaderButton-options">
 			<a href="myAuction.php">View</a>
@@ -78,15 +100,16 @@ require 'dbConn.php';
 	<span class="HeaderButton">
 		<button>Admin</button>
 		<div class="HeaderButton-options">
-			<a href="adminCategories.php">Show-Categories</a>
-			<!-- <a href="addCategories.php">Delete-Categories</a> -->
-			<a href="addCategories.php">Add-Categories</a>
-			<a href="logout.php">logout</a>
+			<a href="adminCategories.php">Show Categories</a>
+			<a href="addCategories.php">Delete Categories</a>
+			<a href="addCategories.php">Add Categories</a>
+
 		</div>
 		</div>
 	</span>
-		<img src="banners/1.jpg" alt="Banner" />
+	<img src="banners/1.jpg" alt="Banner" />
 
 
-	</body>
+</body>
+
 </html>
--- websites/as1/public/register.php
+++ websites/as1/public/register.php
@@ -1,40 +1,41 @@
-<?php 
+<?php
 require 'header.php';
 require 'dbConn.php'
-?>
+    ?>
 
 <!-- Registration Form -->
- <h1>Registration</h1>
+<h1>Registration</h1>
 
- <form action="#" method="POST">
-     <label for="username">Username: </label>
-     <input type="text" name="username" required /><br>
-     <label for="email">Email: </label>
-     <input type="email" name="email" required /><br>
-     <label for="password">Password: </label>
-     <input type="password" name="password" required><br>
-     
+<form action="#" method="POST">
+    <label for="username">Username: </label>
+    <input type="text" name="username" required /><br>
+    <label for="email">Email: </label>
+    <input type="email" name="email" required /><br>
+    <label for="password">Password: </label>
+    <input type="password" name="password" required><br>
 
-     <!-- For Admins -->
-     <input type="radio" name="role" value="user"/> <label>user</label>
-    <input type="radio" name="role" value="admin"/> <label>admin</label>
+
+    <!-- For Admins -->
+    <input type="radio" name="role" value="user" /> <label>user</label>
+    <input type="radio" name="role" value="admin" /> <label>admin</label>
     <input type="submit" value="Submit" name="submit" /><br><br>
- </form>
+</form>
 
- <!-- Post userdata to DB -->
- <?php
-if(isset($_POST['submit'])){
+<!-- Post userdata to DB -->
+<?php
+if (isset($_POST['submit'])) {
     $name = $_POST['username'];
     $email = $_POST['email'];
     $role = $_POST['role'];
     $passw = sha1($_POST['password']);
-    
+
     $result = $conn->query("INSERT INTO `users`(`name`, `email`, `password`, `role`) VALUES ('$name','$email','$passw','$role')");
-    echo 'Thank you for registering <br/> <a href="login.php"><input type="submit" value="Login Now" name="loginBtn" /> </a>';
+    echo 'Thank you for registering  ';
 }
 ?>
 
-Already Have an Account ?
+<br><br>
+<p>Already Have an Account ?</p>
 <a href="login.php"><button>Login Now</button></a>
 <?php
 require 'footer.php';
