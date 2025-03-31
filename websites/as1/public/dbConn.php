e8dae3b661ff305b459e599d69a9e124b0588f8f
21subham <subhamrajbhandari@gmail.com>
2025-03-31 16:59:28 +0545

    bidauction huna lagyo

--- websites/as1/public/dbConn.php
+++ websites/as1/public/dbConn.php
@@ -1,19 +1,19 @@
 <?php
 
-    $host = "localhost";
-    $db_name = "assignment1";
-     $username = "v.je";
-     $password = "v.je";
-    $conn;
- 
-   
-      $conn = new PDO('mysql:dbname=assignment1;host=mysql', $username, $password,[PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION]);
- 
-        
-    $getCategoryQuery = $conn->prepare('SELECT * FROM category');
-    $getCategoryQuery->execute();
-    $getCategory = $getCategoryQuery->fetchAll();  
-       
-    
+$host = "localhost";
+$db_name = "assignment1";
+$username = "v.je";
+$password = "v.je";
+$conn;
+
+
+$conn = new PDO('mysql:dbname=assignment1;host=mysql', $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+
+$getCategoryQuery = $conn->prepare('SELECT * FROM category');
+$getCategoryQuery->execute();
+$getCategory = $getCategoryQuery->fetchAll();
+
+
 
 ?>
